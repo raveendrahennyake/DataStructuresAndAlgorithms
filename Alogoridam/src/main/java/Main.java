5af54b25e63e9e67202bb2fad8f2697d766f6f3c
raveendrahennyake <raveendralakshan5@gmail.com>
2024-04-15 22:20:07 +0530

    Initial commit

---
+++ Alogoridam/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.example.App;
+public class Main {
+    public static void main(String[] args) {
+
+     int [] arr={88,99,22,44,66};
+
+        for (int num :arr){
+            System.out.println(num+"");
+        }
+
+
+
+    }
+
+
+
+
+    }
+
+
+
+
+
---
+++ Alogoridam/src/main/java/org/example/App.java
@@ -0,0 +1,56 @@
+package org.example;
+import static org.example.App.quickSort;
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        int[] arr = {10, 6, 11, 8, 12, 2, 9, 15};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low + 1;
+        int j = high;
+
+        while (i <= j) {
+            while (i <= j && arr[i] <= pivot) {
+                i++;
+            }
+            while (i <= j && arr[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+
+        return j;
+
+
+
+    }
+
+
+}
+
+
