809c64b6cdf69be8ce4bd5a33e3f7777198b2b21
raveendrahennyake <raveendralakshan5@gmail.com>
2024-04-16 20:57:42 +0530

    git commit -m 'message2'

--- Alogoridam/src/main/java/Main.java
+++ Alogoridam/src/main/java/Main.java
@@ -4,18 +4,25 @@ public class Main {
 
      int [] arr={88,99,22,44,66};
 
+     QuickSort (arr,0,arr.length-1);
+
         for (int num :arr){
             System.out.println(num+"");
         }
+    }
+   public static void QuickSort(int[] arr, int start, int end) {
+        if (end>start);
 
 
+        int pivate= partition(arr,start,end);
+    }
+
+    public static int  partition(int[] arr, int start, int end){
+        return start;
 
     }
 
-
-
-
-    }
+}
 
 
 
---
+++ Alogoridam/src/main/java/org/example/Sortin.java
@@ -0,0 +1,32 @@
+package org.example;
+    public class Sortin {
+        public static void main(String[] args) {
+            int[] arr = {2, 6, 7, 8, 10, 12, 9};
+            selectionSort(arr);
+            System.out.println("Sorted array:");
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+        }
+
+        public static void selectionSort(int[] arr) {
+            int n = arr.length;
+
+            for (int i = 0; i < n - 1; i++) {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i) {
+                    int temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                }
+            }
+        }
+    }
+
+
