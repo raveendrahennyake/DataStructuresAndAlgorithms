fe38b7f69975932f10f7b5abfc4fd828438c8281
raveendrahennyake <raveendralakshan5@gmail.com>
2024-04-20 18:44:44 +0530

    git commit -m 'merge'

---
+++ Alogoridam/src/main/java/org/example/Student.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Student {
+    public static void main(String[] args) {
+        int [] arr={12, 6, 7, 8, 10, 2, 9};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n=arr.length;
+        for (int i=1;i<n;i+=1){
+            int key=arr[i];
+
+
+        }
+
+    }
+
+
+}
+
